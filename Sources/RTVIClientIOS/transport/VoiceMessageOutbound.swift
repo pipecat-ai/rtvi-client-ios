6298c2a0810d73d357dcf56cfe9d1f32d483afd2
Filipi Fuchter <filipi87@gmail.com>
2024-09-03 09:00:20 -0300

    Allowing to pass if we should interrupt or not when updating config.

--- Sources/RTVIClientIOS/transport/VoiceMessageOutbound.swift
+++ Sources/RTVIClientIOS/transport/VoiceMessageOutbound.swift
@@ -2,12 +2,12 @@ import Foundation
 
 /// An RTVI control message sent to the Transport.
 public struct VoiceMessageOutbound: Encodable {
-    
+
     let id: String
     let label: String
     let type: String
     let data: Value?
-    
+
     /// Messages from the client to the server.
     public enum MessageType {
         static let UPDATE_CONFIG = "update-config"
@@ -17,42 +17,42 @@ public struct VoiceMessageOutbound: Encodable {
         static let DESCRIBE_ACTIONS = "describe-actions"
         public static let CLIENT_READY = "client-ready"
     }
-    
+
     public init(id: String = String (UUID().uuidString.prefix(8)), label: String = "rtvi-ai", type: String, data: Value? = nil) {
         self.id = id
         self.label = label
         self.type = type
         self.data = data
     }
-    
+
     static func action(actionData: ActionRequest) async throws -> VoiceMessageOutbound {
         return VoiceMessageOutbound(
             type: VoiceMessageOutbound.MessageType.ACTION,
             data: try await actionData.convertToRtviValue()
         )
     }
-    
-    static func updateConfig(config: [ServiceConfig]) async throws -> VoiceMessageOutbound {
+
+    static func updateConfig(config: [ServiceConfig], interrupt: Bool) async throws -> VoiceMessageOutbound {
         let configAsValue = try await config.convertToRtviValue()
-        let data = Value.object(["config": configAsValue])
+        let data = Value.object(["config": configAsValue, "interrupt": Value.boolean(interrupt)])
         return VoiceMessageOutbound(
             type: VoiceMessageOutbound.MessageType.UPDATE_CONFIG,
             data: data
         )
     }
-    
+
     static func describeConfig() -> VoiceMessageOutbound {
         return VoiceMessageOutbound(
             type: VoiceMessageOutbound.MessageType.DESCRIBE_CONFIG
         )
     }
-    
+
     static func getConfig() -> VoiceMessageOutbound {
         return VoiceMessageOutbound(
             type: VoiceMessageOutbound.MessageType.GET_CONFIG
         )
     }
-    
+
     static func describeActions() -> VoiceMessageOutbound {
         return VoiceMessageOutbound(
             type: VoiceMessageOutbound.MessageType.DESCRIBE_ACTIONS
